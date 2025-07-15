ba3669d4e29b23f420d5d329083d62e996310199
Tanshi Mehra <tanshimehra547@gmail.com>
2025-07-15 11:58:42 +0530

    Frontend

---
+++ Frontend Test Submission/src/App.jsx
@@ -0,0 +1,12 @@
+import HomePage from "./pages/HomePage";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Frontend Evaluation</h1>
+      <HomePage />
+    </div>
+  );
+};
+
+export default App;
