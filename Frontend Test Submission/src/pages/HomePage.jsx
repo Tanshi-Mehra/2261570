8432bf98cd9ddf0b9a87dd50f36d03df4517a1cd
Tanshi Mehra <tanshimehra547@gmail.com>
2025-07-15 12:00:05 +0530

    home page

---
+++ Frontend Test Submission/src/pages/HomePage.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { logEvent } from "../../../Logging Middleware/logger";
+import UserInfo from "../components/UserInfo";
+
+const HomePage = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    logEvent("info", "page", "HomePage mounted");
+
+    const fetchData = async () => {
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/users/1");
+        const data = await res.json();
+        setUser(data);
+        logEvent("info", "api", "Fetched user successfully");
+      } catch (err) {
+        logEvent("error", "api", "Failed to fetch user");
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h2>Home Page</h2>
+      {user ? <UserInfo user={user} /> : <p>Loading user...</p>}
+    </div>
+  );
+};
+
+export default HomePage;
