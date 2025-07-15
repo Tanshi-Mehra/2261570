096d21d6857c37045bab461d22f845f43c211229
Tanshi Mehra <tanshimehra547@gmail.com>
2025-07-15 12:13:10 +0530

    modified

--- Frontend Test Submission/src/App.jsx
+++ Frontend Test Submission/src/App.jsx
@@ -1,12 +1,20 @@
-import HomePage from "./pages/HomePage";
+// src/App.jsx
+import React, { useEffect } from 'react';
+import { logEvent } from './middleware/logger';
 
-const App = () => {
+function App() {
+  useEffect(() => {
+    logEvent("info", "page", "App component mounted");
+  }, []);
+
   return (
     <div>
-      <h1>Frontend Evaluation</h1>
-      <HomePage />
+      <h1>Hello React!</h1>
+      <button onClick={() => logEvent("warn", "component", "Button clicked!")}>
+        Log Warning
+      </button>
     </div>
   );
-};
+}
 
 export default App;
